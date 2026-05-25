02f21bee94e926247eb11685756d1f02f5902256
syfslfk00-debug <syfslfk00@gmail.com>
2026-05-25 21:49:20 +0000

    رفع الكود بعد تنظيف التاريخ

---
+++ events/ready.js
@@ -0,0 +1,18 @@
+const { Client, ActivityType, Events } = require("discord.js");
+
+module.exports = {
+    name: Events.ClientReady,
+    once: true,
+    /**
+     * @param {Client} client
+     */
+    execute(client) {
+        client.user.setStatus("dnd");
+        client.user.setActivity({
+            name: 'by onlyzer0x', 
+            type: ActivityType.Playing, 
+        });
+        
+        console.log(`Bot is now online as ${client.user.tag}`);
+    },
+};
