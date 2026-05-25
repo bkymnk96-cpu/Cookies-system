4796f1b4d3b9ab17261ab77f50174650d0905c49
syfslfk00-debug <syfslfk00@gmail.com>
2026-05-26 01:00:36 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // التوكن محمي ويقرأ من متغيرات البيئة في Railway
+  token: process.env.DISCORD_TOKEN,
+  
+  prefix: "!",
+  
+  // ضع الأيدي الخاص بك هنا مباشرة بين علامتي التنصيص
+  owner: "656783724662226963"
+};
