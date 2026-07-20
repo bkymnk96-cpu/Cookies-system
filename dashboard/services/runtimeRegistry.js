a53a485bf9e68ea46d560c490594bb1eed2c4e02
bkymnk96-cpu <bkymnk96@gmail.com>
2026-07-20 17:54:49 +0300

    Delete dashboard/services directory

--- dashboard/services/runtimeRegistry.js
+++
@@ -1,5 +0,0 @@
-let client = null;
-function registerClient(discordClient) { client = discordClient; }
-function getClient() { return client; }
-function invalidateGuildConfig(guildId) { if (client) client.emit('zeus:dashboardConfigChanged', guildId); }
-module.exports = { registerClient, getClient, invalidateGuildConfig };
